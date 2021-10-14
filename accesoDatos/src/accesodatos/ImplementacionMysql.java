6fb0eaf66678f4f8241ba45e412096963d14d4aa
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-14 10:26:25 +0200

    Create ImplementacionMysql.java

---
+++ accesoDatos/src/accesodatos/ImplementacionMysql.java
@@ -0,0 +1,23 @@
+
+package accesodatos;
+
+/*me encuentro justo antes de la BBDD, 
+vamos a definir los comportamientos de los métodos CRUD*/
+public class ImplementacionMysql implements IAccesoDatos {
+
+    @Override
+    public void insertar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actualizar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
