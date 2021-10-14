d9a0afacb77f2eaa410980393bdc87a5c05d5752
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-14 10:26:27 +0200

    Create IAccesoDatos.java

---
+++ accesoDatos/src/accesodatos/IAccesoDatos.java
@@ -0,0 +1,14 @@
+package accesodatos;
+
+public interface IAccesoDatos {
+
+    int MAX_REGISTRO = 10; //aunque no lo defina es static y final
+
+//métodos, son abstractos y públicos por defecto
+    void insertar();
+
+    void listar();
+
+    void actualizar();
+
+}
