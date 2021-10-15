d8e78062e5070dbce1d9766c03c7dc70220f50ff
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-10-15 11:34:34 +0200

    Update ImplementacionOracle.java

--- accesoDatos/src/accesodatos/ImplementacionOracle.java
+++ accesoDatos/src/accesodatos/ImplementacionOracle.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package accesodatos;
 
-/**
- *
- * @author Alumno Mañana
- */
 public class ImplementacionOracle implements IAccesoDatos {
 
     @Override
